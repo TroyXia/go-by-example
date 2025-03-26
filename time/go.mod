ff1cf239b1b733bb011e237bf7ebfdfe313f180f
hxia <xiahuyun043@126.com>
2025-03-27 00:50:24 +0800

    add ticker as an example

---
+++ time/go.mod
@@ -0,0 +1,5 @@
+module time
+
+go 1.23.6
+
+require k8s.io/utils v0.0.0-20250321185631-1f6e0b77f77e
---
+++ time/ticker/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"k8s.io/utils/clock"
+)
+
+func main() {
+	// 创建一个实际的时钟实例
+	realClock := clock.RealClock{}
+
+	// 设置定时器的延迟时间为 5 秒
+	delay := 5 * time.Second
+
+	// 使用时钟创建一个定时器
+	timer := realClock.NewTimer(delay)
+
+	fmt.Println("定时器已启动，等待 5 秒...")
+
+	// 等待定时器触发
+	<-timer.C()
+	fmt.Println("定时器触发，2 秒已过。")
+
+	// 重置定时器，再次设置延迟时间为 1 秒
+	timer.Reset(1 * time.Second)
+	fmt.Println("定时器已重置，等待 1 秒...")
+
+	// 再次等待定时器触发
+	<-timer.C()
+	fmt.Println("定时器再次触发，1 秒已过。")
+
+	// 停止定时器
+	timer.Stop()
+	fmt.Println("定时器已停止。")
+
+	// 再次等待定时器触发
+	timer.Reset(3 * time.Second)
+	fmt.Println("重置定时器，等待 3 秒...")
+	ticker := timer.C()
+	<-ticker
+	fmt.Println("定时器再次触发，3 秒已过。")
+}
